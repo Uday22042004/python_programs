625f124d3d275c64e9ae5642f9165b40c9423d8e
Uday22042004 <udaykelodiya1111@gmail.com>
2023-08-04 22:56:11 +0530

    day 4

---
+++ ishank.py
@@ -0,0 +1,40 @@
+#I writing a code to find out the area of circle first I am writing for predifine value and afterward take value from user 
+# r = 234
+# area = 3.14 * r * r
+
+# print(area)
+# print("Enter the value of r = ") 
+# r=int(input())
+
+# area = 3.14* r * r
+# print(" your area of circle is ")
+# print(area)
+# str1 = "Thank" 
+# str2= "You"
+# print(str1 + " " + str2 )
+# a=34+38+39+39+38+36+37+37+0+39+39+36+38+40+38+37+35+39+38+37+38+39+38+29+39+0+39+38+35
+# b=34+3737+39+36+37+36+37+37+37+41+37+35+36+38+40+41+35+37+37+38+36+40+39+39+39+40
+# print(a)
+# print(b)
+# print(a+b)
+
+a=2
+b=3
+print(a ** b)# issme yee 2 ki power 3 hai
+
+
+# n=int(input())
+
+# for i in range(n):
+#     print(i**2)
+    
+# Creating an empty dictionary
+my_dict = {}
+
+# Adding key-value pairs to the dictionary
+my_dict[input()] = "value1"
+my_dict["key2"] = "value2"
+my_dict["key3"] = "value3"
+
+# Printing the dictionary
+print(my_dict)
