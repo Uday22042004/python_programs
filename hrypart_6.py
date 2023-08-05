bbb95a461ce17596cb0b623f6d48828ee48f24d3
Uday22042004 <udaykelodiya1111@gmail.com>
2023-08-05 16:44:34 +0530

    day 6

---
+++ hrypart_6.py
@@ -0,0 +1,57 @@
+#In this file we discuss about loops and haave some example just for practice
+# 
+
+#This is an example for for loop
+# x=1
+# for i in range(0,20001):
+#     print(i)
+
+# colors = ["Red","Green","White","Yellow","blue"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+#         if (i=="e"):
+#             print("E is everywhere every one have e")
+#         elif(i=="R"):
+#             print("Starting alphabat of color")
+#         else:
+#             print(" ")
+    
+    
+# # This an example for While loop
+# x=0
+# while (x<9):
+#     print(x)
+#     x=x+1
+    
+# count=9 
+# while(count<=0):
+#     print(count)
+#     count=count-1
+
+#Here we are using break statement and continue statement
+# table=int(input("Enter the number of table you want : "))
+table=2
+num=0
+while(True):
+    if (num==8):
+        2
+        continue
+    print(table,"x",num+1,"=",table*(num+1))
+    num=num+1
+    
+    if (num==10):
+
+        break
+    
+    
+    
+print("Thanks")
+
+#here we are taking an example of continue statement
+
+    
+      
+   
+
